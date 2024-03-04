74a3a47358e9716263d20d1265d0d2b77f53943f
Laura <amaxranthine1995@gmail.com>
2024-03-04 11:49:30 +0100

    variables de session

--- Exercici_2/formulario.php
+++ Exercici_2/formulario.php
@@ -1,2 +1,19 @@
 //tenemos que iniciar la sesión
 session_start();
+
+
+// Verificar si se han recibido datos del formularo
+<?php
+// variables superglobals
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  $nombre = $_POST ['nombre']; 
+  $email = $_POST ['email'];
+  $user = $_POST ['user'];
+
+  // Display a greeting message with the entered name
+echo "<p> Nombre :$nombre </p> ";
+echo "<p> Email :$email </p> ";
+}
+$_SESSION ['user'] = $user;
+echo $_SESSION ['user'];
+?>
