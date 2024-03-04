273a0ce9384cb2a030e7e0500724b0ed5946f508
Laura <amaxranthine1995@gmail.com>
2024-03-04 11:25:36 +0100

    creado doc php

--- Exercici_1/Formulario.php
+++ Exercici_1/Formulario.php
@@ -1,3 +1,12 @@
 <?php
 // variables superglobals
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  $nombre = $_POST ['nombre']; 
+  $email = $_POST ['email'];
+
+  // Display a greeting message with the entered name
+echo "<p> Nombre :$nombre </p> ";
+echo "<p> Email :$email </p> ";
+}
+
 ?>
